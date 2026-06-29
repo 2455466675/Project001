5f1364d882fd72a3be5f2572a44baabc758b5bc4
lichuang <2455466675@qq.com>
2026-06-29 19:06:17 +0800

    回合循环

--- Client/Assets/Scripts/HotUpdate/Logic/Gameplay/Battle/BattleModule.cs
+++ Client/Assets/Scripts/HotUpdate/Logic/Gameplay/Battle/BattleModule.cs
@@ -48,6 +48,35 @@ namespace GameFramework.Logic
             Game.GetSystem<GameCameraController>().SetCameraModel(CameraModel.Controlled);
             Game.Message.SendMessage(new UIPanelMessage() { isVisible = true, panel = Utility.GameDefine.PanelDefine.BattleFormation });
             await UniTask.CompletedTask;
+    
+            /*
+            *加载阵形（角色配置、位置）
+            *创建对象
+            
+            流程：
+            1、入场阶段（对象入场，播放入场动画）                
+            2、回合循环 (Loop)
+            {
+                单回合阶段：
+                {
+                    1、回合开始阶段
+                    2、回合进行阶段：
+                    {   
+                        1、确定行动对象
+                        2、等待对象行动：
+                        {
+                            1、行动前事件
+                            2、选择行为（玩家输入 or AI）
+                            3、等待行为结算
+                            4、行动结束事件
+                        }
+                        3、下一个对象行动（行动队列为空，回合结束）                        
+                    }
+                    3、回合结束阶段
+                }
+            }
+            3、结算阶段
+            */
         }
 
         private async UniTask UnloadBattle()
